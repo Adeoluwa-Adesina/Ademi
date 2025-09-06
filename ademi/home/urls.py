929be555ad63cbafe120b338a63143e2b99f77b3
Adeoluwa Adesina <155592776+Adeoluwa-Adesina@users.noreply.github.com>
2025-09-06 20:39:17 +0100

    Delete ademi directory

--- ademi/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="home"),
-    path('home/', views.index, name="home"),
-    path('base/', views.base, name="base"),
-]
